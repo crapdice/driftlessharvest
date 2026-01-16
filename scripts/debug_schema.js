fecc117738c24bc0e9a9db5e3625b2ffe789ebf5
Your Name <you@local.dev>
2026-01-16 11:37:20 -0600

    Pre-admin refactor checkpoint: filesystem cleanup complete

--- scripts/debug_schema.js
+++
@@ -1,9 +0,0 @@
-const db = require('better-sqlite3')('server/db/harvest.db');
-
-console.log("--- Products Table Schema ---");
-const columns = db.prepare("PRAGMA table_info(products)").all();
-console.log(JSON.stringify(columns, null, 2));
-
-console.log("\n--- Categories Table Schema ---");
-const catCols = db.prepare("PRAGMA table_info(categories)").all();
-console.log(JSON.stringify(catCols, null, 2));
